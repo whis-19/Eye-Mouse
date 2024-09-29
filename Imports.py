576d0380e93b37944ce7d820c29e574c8c256cd9
Muhammad Abdullah <ma3472995@gmail.com>
2024-09-30 01:04:05 +0500

    minor fixes

--- Imports.py
+++ Imports.py
@@ -8,11 +8,7 @@ import keyboard
 
 
 
-
-# Control loop variable
-
-# Initialize video capture and face mesh
 cam = cv2.VideoCapture(0)
 faceMesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 screenWidth, screenHeight = pyautogui.size()
-print("Screen Width:", screenWidth, "Screen Height:", screenHeight)  # Debugging screen size
+print("Screen Width:", screenWidth, "Screen Height:", screenHeight) 
