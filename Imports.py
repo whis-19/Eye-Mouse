100536f9d4db3ae10ff6dafa326088667eb2a107
Muhammad Abdullah <ma3472995@gmail.com>
2024-11-26 17:57:20 +0500

    Revert "minor fixes"
    This reverts commit 576d0380e93b37944ce7d820c29e574c8c256cd9.

--- Imports.py
+++ Imports.py
@@ -8,7 +8,11 @@ import keyboard
 
 
 
+
+# Control loop variable
+
+# Initialize video capture and face mesh
 cam = cv2.VideoCapture(0)
 faceMesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 screenWidth, screenHeight = pyautogui.size()
-print("Screen Width:", screenWidth, "Screen Height:", screenHeight) 
+print("Screen Width:", screenWidth, "Screen Height:", screenHeight)  # Debugging screen size
